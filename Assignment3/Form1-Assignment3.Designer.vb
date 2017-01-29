393f3552600e8a12362ec2da4190f2a9a6d6cda9
jovanidash21 <jwardash21@gmail.com>
2017-01-29 18:01:08 +0800

    Update Form1 Design

--- Assignment3/Form1-Assignment3.Designer.vb
+++ Assignment3/Form1-Assignment3.Designer.vb
@@ -22,9 +22,69 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.AssignmentLabel = New System.Windows.Forms.Label()
+        Me.ArrayButton = New System.Windows.Forms.Button()
+        Me.CollectionButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'AssignmentLabel
+        '
+        Me.AssignmentLabel.AutoSize = True
+        Me.AssignmentLabel.Font = New System.Drawing.Font("Cooper Black", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.AssignmentLabel.ForeColor = System.Drawing.Color.FromArgb(CType(CType(231, Byte), Integer), CType(CType(223, Byte), Integer), CType(CType(221, Byte), Integer))
+        Me.AssignmentLabel.Location = New System.Drawing.Point(26, 126)
+        Me.AssignmentLabel.Name = "AssignmentLabel"
+        Me.AssignmentLabel.Size = New System.Drawing.Size(434, 55)
+        Me.AssignmentLabel.TabIndex = 0
+        Me.AssignmentLabel.Text = "Assignment No 3"
+        '
+        'ArrayButton
+        '
+        Me.ArrayButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(162, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(202, Byte), Integer))
+        Me.ArrayButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(231, Byte), Integer), CType(CType(223, Byte), Integer), CType(CType(221, Byte), Integer))
+        Me.ArrayButton.FlatAppearance.BorderSize = 3
+        Me.ArrayButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.ArrayButton.Font = New System.Drawing.Font("Cooper Black", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ArrayButton.ForeColor = System.Drawing.Color.FromArgb(CType(CType(231, Byte), Integer), CType(CType(223, Byte), Integer), CType(CType(221, Byte), Integer))
+        Me.ArrayButton.Location = New System.Drawing.Point(100, 206)
+        Me.ArrayButton.Name = "ArrayButton"
+        Me.ArrayButton.Size = New System.Drawing.Size(265, 69)
+        Me.ArrayButton.TabIndex = 1
+        Me.ArrayButton.Text = "Array"
+        Me.ArrayButton.UseVisualStyleBackColor = False
+        '
+        'CollectionButton
+        '
+        Me.CollectionButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(162, Byte), Integer), CType(CType(57, Byte), Integer), CType(CType(202, Byte), Integer))
+        Me.CollectionButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(231, Byte), Integer), CType(CType(223, Byte), Integer), CType(CType(221, Byte), Integer))
+        Me.CollectionButton.FlatAppearance.BorderSize = 3
+        Me.CollectionButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.CollectionButton.Font = New System.Drawing.Font("Cooper Black", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CollectionButton.ForeColor = System.Drawing.Color.FromArgb(CType(CType(231, Byte), Integer), CType(CType(223, Byte), Integer), CType(CType(221, Byte), Integer))
+        Me.CollectionButton.Location = New System.Drawing.Point(100, 293)
+        Me.CollectionButton.Name = "CollectionButton"
+        Me.CollectionButton.Size = New System.Drawing.Size(265, 69)
+        Me.CollectionButton.TabIndex = 2
+        Me.CollectionButton.Text = "Collection"
+        Me.CollectionButton.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(14, Byte), Integer), CType(CType(11, Byte), Integer), CType(CType(22, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(484, 461)
+        Me.Controls.Add(Me.CollectionButton)
+        Me.Controls.Add(Me.ArrayButton)
+        Me.Controls.Add(Me.AssignmentLabel)
+        Me.Name = "Form1"
+        Me.Text = "Assignment No. 3"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents AssignmentLabel As Label
+    Friend WithEvents ArrayButton As Button
+    Friend WithEvents CollectionButton As Button
 End Class
